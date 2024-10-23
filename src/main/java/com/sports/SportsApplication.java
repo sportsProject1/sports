649befc4bc948efe1d0f11a4d36914a7a71e5446
WYJ5032 <dudwns5032@gmail.com>
2024-10-23 15:39:23 +0900

    임시

--- src/main/java/com/sports/SportsApplication.java
+++ src/main/java/com/sports/SportsApplication.java
@@ -3,15 +3,16 @@ package com.sports;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SportsApplication {
 
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        String dbUser = dotenv.get("SPRING_DATASOURCE_USERNAME");
-        String dbPassword = dotenv.get("SPRING_DATASOURCE_PASSWORD");
+//        Dotenv dotenv = Dotenv.load();
+//        String dbUser = dotenv.get("SPRING_DATASOURCE_USERNAME");
+//        String dbPassword = dotenv.get("SPRING_DATASOURCE_PASSWORD");
 
         SpringApplication.run(SportsApplication.class, args);
     }
