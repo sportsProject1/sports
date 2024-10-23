983683bef2d1518f19c40c86f64c4dafe001b55b
WYJ5032 <dudwns5032@gmail.com>
2024-10-23 17:22:28 +0900

    이미지부분 추가

--- src/main/java/com/sports/Item/ItemController.java
+++ src/main/java/com/sports/Item/ItemController.java
@@ -1,9 +1,7 @@
 package com.sports.Item;
 
-import com.sports.Category.Category;
 import com.sports.Category.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
