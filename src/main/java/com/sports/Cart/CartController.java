b1f924156a7ea385ca49b3f2a3e7974363cd66fe
WYJ5032 <dudwns5032@gmail.com>
2024-10-24 17:25:42 +0900

    장바구니 ì 테이블 추가

---
+++ src/main/java/com/sports/Cart/CartController.java
@@ -0,0 +1,11 @@
+package com.sports.Cart;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
+public class CartController {
+}
