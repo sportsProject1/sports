ba3eade2a9e0520ba5f5fbdbe0c5de9540f9853e
ha0goo <likethat0923@gmail.com>
2024-10-24 12:26:58 +0900

    머지전 수정

--- src/main/java/com/sports/user/UserController.java
+++ src/main/java/com/sports/user/UserController.java
@@ -9,9 +9,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
-
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class UserController {
