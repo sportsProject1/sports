d35d8da53a0219e51892d9db80adc5779e47a8e5
WYJ5032 <dudwns5032@gmail.com>
2024-10-24 09:06:15 +0900

    ItemController정리

--- src/main/java/com/sports/Item/ItemController.java
+++ src/main/java/com/sports/Item/ItemController.java
@@ -3,14 +3,6 @@ package com.sports.Item;
 import com.sports.Category.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
-
-@Controller
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -44,15 +36,6 @@ public class ItemController {
     }
 
     @GetMapping("/post")
-    public String postItem(Model model){
-
-        model.addAttribute("categories", categoryService.getAllCategories());
-
-        return "shopAdd";
-
-    }
-
-
     public String postItem(Model m){
 
         m.addAttribute("categories", categoryService.getAllCategories());
