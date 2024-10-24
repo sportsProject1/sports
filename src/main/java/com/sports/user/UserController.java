11520b201ca5d1e3dcee00fc2e57b26559221c02
WYJ5032 <dudwns5032@gmail.com>
2024-10-24 09:19:15 +0900

    시큐리티 최신화

--- src/main/java/com/sports/user/UserController.java
+++ src/main/java/com/sports/user/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/member")
 @RequiredArgsConstructor
 public class UserController {
 
