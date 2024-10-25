c2c1ab3792b3f430323e3a90d036c52391aa2b1a
ha0goo <likethat0923@gmail.com>
2024-10-25 17:57:38 +0900

    껏다켜기전

---
+++ src/main/java/com/sports/Security/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.sports.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/**") // JWT 인증이 필요한 경로
+                .excludePathPatterns("/", "/register", "/login", "/user", "/shop", "/shop/**"); // 예외 경로
+    }
+}
