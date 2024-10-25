a2e831db1a228eb67156d8b6c5b7d42f01ea5d68
WYJ5032 <dudwns5032@gmail.com>
2024-10-25 16:17:16 +0900

    장바구니 일단 넣는부분까지

--- src/main/java/com/sports/Cart/CartController.java
+++ src/main/java/com/sports/Cart/CartController.java
@@ -19,20 +19,20 @@ public class CartController {
 
     @PostMapping("/cart/add/{id}")
     public ResponseEntity<CartDTO> addItemToCart(@PathVariable Long id, @RequestParam Integer count) {
-        ItemDTO itemDTO = itemService.getItemDetail(id); // 아이템 정보를 가져옴
+        ItemDTO itemDTO = itemService.getItemDetail(id);
 
         if (itemDTO != null) {
             Cart cart = new Cart();
 
             // 유저 정보 가져오기
-//            cart.setUserId(로그인된 정보에서 가져오기); // 사용자 ID 설정
-//            cart.setUsername(로그인된 정보에서 가져오기); // 사용자 이름 설정
-            cart.setItem(itemService.getItemEntity(id)); // 아이템 엔티티 설정
+//            cart.setUserId(로그인된 정보에서 가져오기);
+//            cart.setUsername(로그인된 정보에서 가져오기);
+            cart.setItem(itemService.getItemEntity(id));
             cart.setCount(count);
 
-            cartService.addCart(cart); // 카트에 아이템 추가
+            cartService.addCart(cart);
 
-            return ResponseEntity.ok(new CartDTO(cart)); // 카트 DTO로 반환
+            return ResponseEntity.ok(new CartDTO(cart));
         } else {
             return ResponseEntity.notFound().build();
         }
