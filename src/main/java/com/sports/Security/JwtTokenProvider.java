eb9dd5b0699b79f779a657d262c207d3e52bf890
ha0goo <likethat0923@gmail.com>
2024-10-27 19:49:52 +0900

    백로그인

--- src/main/java/com/sports/Security/JwtTokenProvider.java
+++ src/main/java/com/sports/Security/JwtTokenProvider.java
@@ -3,17 +3,23 @@ package com.sports.Security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
 
-    @Value("${jwt.secret}")
-    private String secretKey;
+    private final SecretKey secretKey;
+
+    public JwtTokenProvider() {
+        // HS256에 맞는 안전한 비밀 키 생성
+        this.secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    }
     @Value("${jwt.validity}")
     private long validityInMilliseconds;
 
