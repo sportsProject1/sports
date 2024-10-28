ffafeadbee73351ced99f2b007f6750e6f08b67d
WYJ5032 <dudwns5032@gmail.com>
2024-10-28 16:14:12 +0900

    CRUD권한추가

--- src/main/java/com/sports/Interface/updatable.java
+++ src/main/java/com/sports/Interface/updatable.java
@@ -4,7 +4,7 @@ import com.sports.Item.ItemDTO;
 import com.sports.user.User;
 
 public interface updatable<T> {
-    void update(Long id, T dto, User user);  // User 객체를 추가
-    void delete(Long id, User user);          // User 객체를 추가
+    void update(Long id, T dto, User user);
+    void delete(Long id, User user);
 }
 
