1a368da5bfc039343494117e034f3c67fc3315ef
WYJ5032 <dudwns5032@gmail.com>
2024-10-29 11:55:32 +0900

    장바구니 작업 중간저장1

--- src/main/java/com/sports/Cart/CartService.java
+++ src/main/java/com/sports/Cart/CartService.java
@@ -9,11 +9,31 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
-    //내용없음
+
+    private final CartRepository cartRepository;
+
+//    public List<CartDTO> getCartItemsByUserId(String userId) {
+//        List<Cart> cartItems = cartRepository.findByUserId(userId);
+//
+//        return cartItems.stream()
+//                .map(this::convertToDto)
+//                .collect(Collectors.toList());
+//    }
+//
+//    private CartDTO convertToDto(Cart cart) {
+//        return CartDTO.builder()
+//                .id(cart.getId())
+//                .itemId(cart.getItem().getId())
+//                .quantity(cart.getQuantity())
+//                .userId(cart.getUser().getId())
+//                .build();
+//    }
 }
 
 
