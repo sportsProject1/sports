a8d5a3ac32326233a054012b4d277974a46c067d
WYJ5032 <dudwns5032@gmail.com>
2024-10-29 09:36:26 +0900

    아침회의

--- src/main/java/com/sports/user/UserDTO.java
+++ src/main/java/com/sports/user/UserDTO.java
@@ -8,16 +8,20 @@ import lombok.Data;
 @Data
 public class UserDTO {
     @NotBlank
-    @Size(min = 3, max = 20, message = "Id : 3자~20자 사이가 아님")
+    @Size(min = 6, max = 20, message = "Id : 6자~20자 사이가 아님")
     private String username;
+    // 한글안들어오게
 
     @NotBlank
     @Size(min = 8, max = 20, message = "비밀번호 : 8자~20자 사이가 아님")
     private String password;
+    //영어+숫자
 
     @NotBlank
-    @Size(min = 3, max = 20, message = "닉네임 : 3자~20자 사이가 아님")
+    @Size(min = 2, max = 20, message = "이름 : 2자~20자 사이가 아님")
     private String nickname;
+    //닉네임말고 이름
+
 
     @NotBlank
     @Size(min = 10, max = 13, message = "전화번호 : 10자~13자 사이가 아님")
@@ -29,6 +33,7 @@ public class UserDTO {
 
     //나중에 api 따와보는걸로...
     private String address;
+
     private String imgURL;
     private String role;
 }
