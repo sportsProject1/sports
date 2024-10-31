14907006e29131c1fc05f7513dd12aadbb2ed221
ha0goo <likethat0923@gmail.com>
2024-10-31 20:30:55 +0900

    jooseok

--- src/main/java/com/sports/Security/AuthResponse.java
+++ src/main/java/com/sports/Security/AuthResponse.java
@@ -14,4 +14,15 @@ public class AuthResponse {
     private String role;
     private String accessToken;
     private String refreshToken;
+    private String errorMessage;
+
+    // 기본적인 생성자 (에러 메세지 제외)
+    public AuthResponse(String username, String nickname, String role, String accessToken, String refreshToken) {
+    }
+
+    // 에러 메세지만 보낼때
+    public AuthResponse(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
 }
