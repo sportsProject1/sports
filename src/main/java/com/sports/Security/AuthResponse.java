9440270cf729af52f0a28a253b730d4c9b2c9ebf
ha0goo <likethat0923@gmail.com>
2024-10-31 20:44:37 +0900

    어스리스폰스 생성자 수정

--- src/main/java/com/sports/Security/AuthResponse.java
+++ src/main/java/com/sports/Security/AuthResponse.java
@@ -18,6 +18,11 @@ public class AuthResponse {
 
     // 기본적인 생성자 (에러 메세지 제외)
     public AuthResponse(String username, String nickname, String role, String accessToken, String refreshToken) {
+        this.username = username;
+        this.nickname = nickname;
+        this.role = role;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
     }
 
     // 에러 메세지만 보낼때
