3c849dd296694564a5800add522bc1d99268e2fb
ha0goo <likethat0923@gmail.com>
2024-11-04 21:20:19 +0900

    AWS RDS Connection

--- src/main/java/com/sports/user/UserDTO.java
+++ src/main/java/com/sports/user/UserDTO.java
@@ -15,33 +15,32 @@ public class UserDTO {
     private Long id;
 
     @NotBlank(groups = ValidationGroups.Create.class)
-    @Size(min = 6, max = 20, message = "Id : 6자~20자 사이가 아님")
-    @Pattern(regexp = "^[a-zA-Z]+[a-zA-Z0-9]*$", message = "Id : 영어만 or 영어+숫자 조합만 허용")
+//    @Size(min = 6, max = 20, message = "Id : 6자~20자 사이가 아님")
+//    @Pattern(regexp = "^[a-zA-Z]+[a-zA-Z0-9]*$", message = "Id : 영어만 or 영어+숫자 조합만 허용")
     private String username;
 
     @NotBlank(groups = ValidationGroups.Create.class)
-    @Size(min = 8, max = 20, message = "비밀번호 : 8자~20자 사이가 아님")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]*$", message = "비밀번호 : 영어+숫자 조합만 허용")
+//    @Size(min = 8, max = 20, message = "비밀번호 : 8자~20자 사이가 아님")
+//    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]*$", message = "비밀번호 : 영어+숫자 조합만 허용")
     private String password;
 
     @NotBlank
-    @Size(min = 2, max = 20, message = "이름 : 2자~20자 사이가 아님")
+//    @Size(min = 2, max = 20, message = "이름 : 2자~20자 사이가 아님")
     private String nickname;
     //닉네임말고 이름
 
     @NotBlank
-    @Size(min = 10, max = 13, message = "전화번호 : 10자~13자 사이가 아님")
+//    @Size(min = 10, max = 13, message = "전화번호 : 10자~13자 사이가 아님")
     private String phone;
 
     @NotBlank
     @Email
     private String email;
+    private String role;
 
     //나중에 api 따와보는걸로...
     private String address;
-
     private String imgURL;
-    private String role;
 
     // 마이페이지 유저 정보 보내기용 생성자
     public UserDTO(Long id, String username, String nickname, String phone, String email, String address, String imgURL, String role) {
