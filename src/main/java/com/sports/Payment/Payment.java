2bf353c530e4c271490a4f3269adc21506f274fa
WYJ5032 <dudwns5032@gmail.com>
2024-11-04 12:13:59 +0900

    상품수정 이미지부분 처리

--- src/main/java/com/sports/Payment/Payment.java
+++ src/main/java/com/sports/Payment/Payment.java
@@ -1,12 +1,10 @@
 package com.sports.Payment;
 
 import com.sports.Cart.Cart;
-import com.sports.user.User;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
