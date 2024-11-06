49866df8cfc27a1dfc2dd4800b629b9fc2d1d77d
Haegi <b_haegi@naver.com>
2024-11-06 11:31:58 +0900

    oauth 수정

--- frontend/src/Utils/OAuth2RedirectHandler.jsx
+++ frontend/src/Utils/OAuth2RedirectHandler.jsx
@@ -1,31 +1,40 @@
-import {useNavigate} from "react-router-dom";
-import {useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import axios from "axios";
 
-function OAuth2RedirectHandler(){
+function OAuth2RedirectHandler() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        const getOAuth2Token = async() => {
-            const Oauth2Token = await axios.get("http://localhost:8090/oauth2/token",{
-                method: "POST",
-                credentials: "include",
-            }).then((res) =>{
-                if(res.ok) {
-                    // Authorization 헤더에서 토큰 가져옴
-                    const token = res.headers.get("accessToken")
-                    const refreshToken = res.headers.get("refreshToken")
-                    console.log(token, refreshToken)
+        const getOAuth2Token = async () => {
+            try {
+                const res = await axios.get("http://localhost:8090/oauth2/token", {
+                    withCredentials: true,
+                });
+
+                if (res.status === 200) {
+                    // Authorization 헤더에서 토큰 가져오기
+                    const token = res.headers["accessToken"];
+                    const refreshToken = res.headers["refreshToken"];
+                    console.log("Access Token:", token);
+                    console.log("Refresh Token:", refreshToken);
+
                     localStorage.setItem("token", token);
                     localStorage.setItem("refreshToken", refreshToken);
-                    navigate("/",{replace:true});
-                }}).catch(err =>{
-                    console.log(err)
+                    navigate("/", { replace: true });
+                } else {
                     navigate("/login");
-                });
-        }
+                }
+            } catch (err) {
+                console.log(err);
+                navigate("/login");
+            }
+        };
+
         getOAuth2Token();
     }, [navigate]);
-    return <div>로딩중</div>
+
+    return <div>로딩중</div>;
 }
+
 export default OAuth2RedirectHandler;
