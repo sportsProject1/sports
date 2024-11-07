94bd45a32b37e7cd4edf6d9356791c77afc63a0f
ha0goo <likethat0923@gmail.com>
2024-11-07 09:59:48 +0900

    OAuth2 get UserInfo and token ok

--- frontend/src/Utils/OAuth2RedirectHandler.jsx
+++ frontend/src/Utils/OAuth2RedirectHandler.jsx
@@ -1,35 +1,43 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import {setCredentials} from "../Store/authSlice";
 
 function OAuth2RedirectHandler() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        const fetchTokens = async () => {
+        const fetchTokensAndUserInfo = async () => {
             try {
-                const response = await axios.get("http://localhost:8090/api/oauth2/token", {
+                const response = await axios.get("http://localhost:8090/oauth2/token", {
                     withCredentials: true,
                 });
 
-                const { accessToken, refreshToken } = response.data;
+                const { accessToken, refreshToken, user } = response.data;
 
-                if (accessToken && refreshToken) {
+                if (accessToken && refreshToken && user) {
+                    // 유저 정보와 토큰을 Redux 스토어와 로컬 스토리지에 저장
+                    dispatch(setCredentials({ user, token: accessToken }));
+
                     localStorage.setItem("accessToken", accessToken);
                     localStorage.setItem("refreshToken", refreshToken);
+                    localStorage.setItem("user", JSON.stringify(user));
+                    localStorage.setItem("loginTime", new Date().getTime());
 
                     navigate("/", { replace: true });
                 } else {
                     navigate("/login");
                 }
             } catch (error) {
-                console.error("토큰 가져오기 실패:", error);
+                console.error("토큰 및 유저 정보 가져오기 실패:", error);
                 navigate("/login");
             }
         };
 
-        fetchTokens();
-    }, [navigate]);
+        fetchTokensAndUserInfo();
+    }, [navigate, dispatch]);
 
     return <div>로딩 중...</div>;
 }
