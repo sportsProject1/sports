31dbf8348690bfd3dc187869254163a733d630fa
ha0goo <likethat0923@gmail.com>
2024-11-18 17:45:29 +0900

    내브바 완성, 회원가입 css

--- frontend/src/Page/user/userComponents/FormFields.jsx
+++ frontend/src/Page/user/userComponents/FormFields.jsx
@@ -9,13 +9,26 @@ const ZipCodeWrapper = styled.div`
     gap: 10px;
     margin-bottom: 20px;
     width: 100%;
+    input {
+        flex: 1;
+    }
+
     button {
+        flex: 0 0 30%;
         border: none;
         color: white;
-        width: 30%;
         background-color: ${({ theme }) => theme.colors.primary};
+        padding: 10px;
+        text-align: center;
+        border-radius: 4px;
+        cursor: pointer;
+        transition: background-color 0.3s;
+
+        &:hover {
+            background-color: ${({ theme }) => theme.colors.secondary};
+        }
     }
-`;
+    `;
 
 const ButtonContainer = styled.div`
     display: flex;
@@ -29,8 +42,21 @@ const RegisterButton = styled.div`
     flex-direction: column;
     gap: 10px;
     margin-top: 20px;
+
     button {
         width: 100%;
+        padding: 12px;
+        border: none;
+        background-color: ${({ theme }) => theme.colors.primary};
+        color: white;
+        border-radius: 4px;
+        font-size: 16px;
+        cursor: pointer;
+        transition: background-color 0.3s;
+
+        &:hover {
+            background-color: ${({ theme }) => theme.colors.secondary};
+        }
     }
 `;
 
