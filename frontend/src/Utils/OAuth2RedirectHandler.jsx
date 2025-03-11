b40498fb9c6018a88a7c2c9c5bb8e15441599bea
Haegi <62898131+haegi-git@users.noreply.github.com>
2025-03-11 16:46:51 +0900

    Update OAuth2RedirectHandler.jsx

--- frontend/src/Utils/OAuth2RedirectHandler.jsx
+++ frontend/src/Utils/OAuth2RedirectHandler.jsx
@@ -11,7 +11,7 @@ function OAuth2RedirectHandler() {
     useEffect(() => {
         const fetchTokensAndUserInfo = async () => {
             try {
-                const response = await axios.get("https://sports-5ebw.onrender.com/oauth2/token", {
+                const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/oauth2/token`, {
                     withCredentials: true,
                 });
 
